d5fe6f60344f76355ec34b20166e605385631442
Starscouts <starscouts@equestria.dev>
2024-07-24 10:51:37 +0200

    Fix issue with variants

--- src/html/items/link.rs
+++ src/html/items/link.rs
@@ -4,34 +4,53 @@ use crate::config::{ItemConfig, ListConfig};
 
 pub fn link(inner: Markup, list: &ListConfig, item: &ItemConfig, price: i32, id: &str) -> Markup {
     html! {
-        a data-recommend=(list.recommend) data-list=(list.title)
-            id={ "item-" (list.title) "-" (id) } data-score="0"
-            data-price=(price)
-            href=(
-                if let Some(link) = &item.link {
-                    link
-                } else {
-                    ""
-                }
-            )
-            class={
-                "list-item list-item-sel list-group-item "
-                (
-                    if let Some(_) = &item.link {
-                        "list-group-item-action "
-                    } else {
-                        " "
-                    }
-                )
-                (
-                    if list.nsfw {
-                        "list-item-nsfw"
-                    } else {
-                        ""
-                    }
-                )
-            } {
-            (inner)
+        @if let Some(link) = &item.link {
+            a data-recommend=(list.recommend) data-list=(list.title)
+                id={ "item-" (list.title) "-" (id) } data-score="0"
+                data-price=(price)
+                href=(link)
+                class={
+                    "list-item list-item-sel list-group-item "
+                    (
+                        if let Some(_) = &item.link {
+                            "list-group-item-action "
+                        } else {
+                            " "
+                        }
+                    )
+                    (
+                        if list.nsfw {
+                            "list-item-nsfw"
+                        } else {
+                            ""
+                        }
+                    )
+                } {
+                (inner)
+            }
+        } else {
+            div data-recommend=(list.recommend) data-list=(list.title)
+                id={ "item-" (list.title) "-" (id) } data-score="0"
+                data-price=(price)
+                class={
+                    "list-item list-item-sel list-group-item "
+                    (
+                        if let Some(_) = &item.link {
+                            "list-group-item-action "
+                        } else {
+                            " "
+                        }
+                    )
+                    (
+                        if list.nsfw {
+                            "list-item-nsfw"
+                        } else {
+                            ""
+                        }
+                    )
+                } {
+                (inner)
+            }
         }
     }
 }
